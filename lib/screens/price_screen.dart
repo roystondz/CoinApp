a8bff35b6225256fa50b9bb1d55bc91131ec2a17
Royston <roystonad2004@gmail.com>
2025-02-24 12:57:30 +0530

    CC listing

--- lib/screens/price_screen.dart
+++ lib/screens/price_screen.dart
@@ -10,6 +10,51 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Coin Today',
+        style: TextStyle(
+          color: Colors.white,
+
+        ),
+        ),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.lightBlueAccent.shade700,
+      ),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.fromLTRB(15.0, 40.0, 15.0, 0),
+            child: Card(
+              color: Colors.blue,
+              elevation: 5.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)
+              ),
+              child: SizedBox(
+                height: 70.0,
+                child: Center(
+                  child: Text('1 BTC =  10 USD',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0
+                  ),),
+                ),
+              ),
+            ),
+          ),
+
+          Container(
+            height: 170,
+            color: Colors.lightBlueAccent.shade700,
+            child: null,
+            alignment: Alignment.center,
+          )
+
+        ],
+      ),
+    );
   }
 }
